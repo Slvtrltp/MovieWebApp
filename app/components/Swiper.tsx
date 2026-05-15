2b70ee9b325c663e99a8a8161ffd4dca1fde5355
Nomin Ganzorig <nominganzorig@Nomins-MacBook-Air-6.local>
2026-05-15 18:41:35 +0800

    aaa

--- app/components/Swiper.tsx
+++ app/components/Swiper.tsx
@@ -53,7 +53,7 @@ export const SwiperM = ({ trend }: { trend: Movie }) => {
         />
       </Link>
 
-      <div className="absolute left-85 z-2 gap-4 top-60 max-[768px]:left-43  ">
+      <div className="absolute left-55 z-2 gap-4 top-60 max-[768px]:left-15  ">
         <div>
           <p className="text-[18px] text-white">Now Playing:</p>
           <Link
